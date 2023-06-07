a70061cb840f59fb811df489baba91c17108a73b
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-06-07 09:02:38 +0200

    added

--- DWA5/dwa5.js
+++ DWA5/dwa5.js
@@ -6,4 +6,20 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  try{
+    if (dividend === "" || divider === "") { 
+      result.innerHTML = "Division not performed. Both values are required in inputs. Try again";
+    }
+    else if (dividend > 0 && divider < 0) { 
+      result.innerHTML = "Division not performed. Invalid number provided. Try again";
+    }
+     else if (isNaN(dividend) && isNaN(divider)) {
+    //   document.body.innerHTML = "Something critical went wrong. Please reload the page";
+    throw document.body.innerHTML = "Something critical went wrong. Please reload the page";
+    }
+  }
+  catch (error) {
+    console.log(error)
+  }
 });
