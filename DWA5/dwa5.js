f5330fb09eddf394c3b20fc897c5ddf40c6ad692
KagisoRabeng <125879959+KagisoRabeng@users.noreply.github.com>
2023-06-07 10:28:39 +0200

    added the throw errors

--- DWA5/dwa5.js
+++ DWA5/dwa5.js
@@ -6,18 +6,20 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+  const wholeNumber = dividend / divider;
 
   try{
     if (dividend === "" || divider === "") { 
-      result.innerHTML = "Division not performed. Both values are required in inputs. Try again";
+      throw result.innerHTML = "Division not performed. Both values are required in inputs. Try again";
     }
     else if (dividend > 0 && divider < 0) { 
-      result.innerHTML = "Division not performed. Invalid number provided. Try again";
+      throw result.innerHTML = "Division not performed. Invalid number provided. Try again";
     }
      else if (isNaN(dividend) && isNaN(divider)) {
     //   document.body.innerHTML = "Something critical went wrong. Please reload the page";
     throw document.body.innerHTML = "Something critical went wrong. Please reload the page";
     }
+    else result.innerText = Math.floor(wholeNumber)
   }
   catch (error) {
     console.log(error)
